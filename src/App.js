49306b65deb7b86f068a23ab9759aac8f8add413
Thien Thanh <nguyenthanhthien0611@gmail.com>
2022-12-18 12:45:11 +0700

    setup react query

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
 import './App.css';
+import { getTODO, postTODO } from './component/fetch';
+import {useQuery,useQueryClient, QueryClient, QueryClientProvider, useMutation } from 'react-query'
+import {ReactQueryDevtools} from 'react-query/devtools'
 
-function App() {
+
+
+function Todos() {
+  // Access the client
+  const queryClient = useQueryClient()
+
+  // Queries
+  const {isLoading, error, data} = useQuery('todos', getTODO)
+  // Mutations
+  const mutation = useMutation(title => postTODO(title), {
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries('todos')
+    },
+  })
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const todo = event.target.newTODO.value;
+    mutation.mutate(todo)
     console.log(todo);
   }
+
+  if (isLoading) return 'Loading...'
+  if (error) return 'An error has occurred: ' + error.message
+  
   return (
-    <form onSubmit={handleSubmit}>
+    <div>
+        
+        <ul>
+         {data.data.map((todo, idx) => (
+           <li key={idx}>{todo}</li>
+         ))}
+       </ul>
+      
+       <form onSubmit={handleSubmit}>
         <input type='text' placeholder="Enter things to do." name='newTODO'/>
-        <button>Add</button>
-    </form>
+        <button>Add TODO</button>
+        </form>
+    </div>
+  )
+}
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    
+    <QueryClientProvider client={queryClient}>
+      <Todos />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+    
+    
   );
 }
 
