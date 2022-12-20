3d6f7a979eb14abe41fb32dbe2ba0520389d3280
thien <s3817852@rmit.edu.vn>
2022-12-20 16:43:35 +0700

    setup todoUpdate

---
+++ functions/todo-update.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+
+const faunadb = require('faunadb')
+const client = new faunadb.Client({secret: process.env.FAUNA_SECRET_KEY})
+
+const {
+    Update,
+    Ref,
+    Collection
+} = faunadb.query
+
+exports.handler = (event, content, callback) => {
+    if(event.httpMethod == 'POST'){
+        const id = event.queryStringParameters.id
+        const title = event.queryStringParameters.title
+        
+        return client.query(
+            Update(
+                Ref(Collection('TODO-List'), id),
+                {
+                  data: {
+                    title: title
+                  },
+                },
+              )
+        ).then((response) => {
+            return {
+                statusCode: 200,
+                headers:{
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+                    'Access-Control-Allow-Methods': "*"
+                },
+                body: JSON.stringify(response)
+            }
+        }).catch(error => {
+            console.log(error)
+            return{
+                statusCode: 400,
+                body: JSON.stringify(error)
+            }
+        })
+         
+    }
+
+    if(event.httpMethod == 'OPTIONS'){
+        return callback(null,{
+            statusCode: 200,
+            headers:{
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+                    'Access-Control-Allow-Methods': "*"
+                },
+            body: JSON.stringify({message: 'Prefight successfully called'})
+        })
+    }
+}
