9387cc0386b412f77d22b0810aff366b0cf17cb1
thien <s3817852@rmit.edu.vn>
2022-12-21 21:50:33 +0700

    adding footer

--- src/component/todoList.js
+++ src/component/todoList.js
@@ -1,7 +1,10 @@
 import { useState} from 'react'
 import { getTODO, postTODO, updateTODO, deleteTODO } from './fetch';
 import {useQuery,useQueryClient, useMutation } from 'react-query';
-import { Typography, AppBar, Container, Box } from '@mui/material';
+import {Copyright,Link, Typography, TextField, Container, Box, Checkbox, Button, List, ListItem,ListItemIcon,ListItemText } from '@mui/material';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import { borderRadius } from '@mui/system';
 
 
 
@@ -70,35 +73,75 @@ export function TodoList() {
     if (error) return 'An error has occurred: ' + error.message
     
     return (
-      <div>
-          <Typography variant="h1">TODO App</Typography>
-          <ul>
-           {data.data.map((todo, idx) => (
-              <TodoItem key={todo[0]}
-                        todoID={todo[0]}
-                        todoINDEX={idx}
-                        value={todo[2]} 
-                        handleDoubleClick={() => setToggled(idx)}
-                        handleDelete={handleDelete}
-                        handleBlur={(e)=> {
-                              e.preventDefault();
-                              const newTitle = e.target.value;
-                              
-                              if(newTitle !== todo[2]){
-                                updateMutation.mutate({index: todo[0], title: newTitle});
-                                setToggled(-1);
-                              }else{
-                                setToggled(-1);
-                              }
-                        }}
-              />
-           ))}
-         </ul>
+      <Container component="main" maxWidth="xs" sx={{backgroundColor: '#f5f5f5'}}>
         
-         <form onSubmit={handleAdd}>
-          <input type='text' placeholder="Enter things to do." name='newTODO'/>
-          <button>Add TODO</button>
-          </form>
-      </div>
+        
+        <Box
+
+          sx={{
+            marginTop: 8,
+            bgcolor: 'background.paper',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            borderColor: 'text.primary',
+            border: 1,
+            borderRadius: '16px'
+          }}
+          >
+            <Typography component="h1" variant="h5" sx={{marginTop: '50px'}}>
+              TODO
+            </Typography>
+            <Box component="span" sx={{ p: 2, border: '1px dashed grey' }}>
+              <Checkbox />a
+            <ul>
+              {data.data.map((todo, idx) => (
+                  <TodoItem key={todo[0]}
+                            todoID={todo[0]}
+                            todoINDEX={idx}
+                            value={todo[2]} 
+                            handleDoubleClick={() => setToggled(idx)}
+                            handleDelete={handleDelete}
+                            handleBlur={(e)=> {
+                                  e.preventDefault();
+                                  const newTitle = e.target.value;
+                                  
+                                  if(newTitle !== todo[2]){
+                                    updateMutation.mutate({index: todo[0], title: newTitle});
+                                    setToggled(-1);
+                                  }else{
+                                    setToggled(-1);
+                                  }
+                            }}
+                  />
+              ))}
+            </ul>
+            </Box>
+            
+            
+            <Box component="form" onSubmit={handleAdd} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="newTODO"
+                label="Enter things todo"
+                type="text"
+              />
+            </Box>
+          </Box>
+
+          {/* Footer  */}
+          <Typography variant='body2' align='center' sx={{ mt: 10 }}>*Double Click To Edit</Typography>
+          <Typography variant="body2" color="text.secondary" align="center" sx={{mt:1,  mb: 4 }}>
+          {'Made with ❤️ '}
+          <Link color="inherit" href="https://github.com/ThienNguyenThanh">
+            Thien Nguyen.
+          </Link>{' '}
+        </Typography>
+
+        
+      </Container>
+      
     )
   }
