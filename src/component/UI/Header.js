2af25d7540fe01e4c8cdb6a8765de8ed4aac8bc3
Thien Thanh <nguyenthanhthien0611@gmail.com>
2022-12-23 12:36:53 +0700

    delete un-used component

--- src/component/UI/Header.js
+++
@@ -1,8 +0,0 @@
-export function Header({ children }) {
-  return (
-    <>
-      <div className="flex flex-col gap-2 sm:flex-row"><h1>Hi</h1></div>
-      <hr className="my-12" />
-    </>
-  );
-}
